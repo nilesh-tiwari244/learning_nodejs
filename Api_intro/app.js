9f67f5c4e2886864eecf73504e11fe02541f7b61
Nilesh Tiwari <nileshtiwari2441996@gmail.com>
2023-01-30 23:48:31 +0530

    middleware completed

--- Api_intro/app.js
+++ Api_intro/app.js
@@ -6,18 +6,26 @@ const logger=require('./middleware1_logger.js');
 const authorize =require('./middleware2_authorize.js');
 
 app.use('/api',[logger,authorize]);
-
+// middleware functions in app.use will be invoked for all within the path criteria
+// to have full control over middleware functions use them locally in routes
+// here also we can have multiple function using array and comma separation
 
 app.get('/',(req,res)=>{
  // res.send('HomePage'); //GET / 2023 as logger is there so this will be consoled
   res.sendFile(path.join(__dirname + '/home.html')); 
 })
 app.get('/about',(req,res)=>{
+  console.log(req.user);
   res.send('About Page'); //GET /about 2023 as as logger is there so this will be consoled
 })
 app.get('/api/docu',(req,res)=>{
+  console.log(req.user);
   res.send('api docu'); //GET /about 2023 as as logger is there so this will be consoled
 })
+app.get('/api/query',(req,res)=>{
+  console.log("here");
+})
+
 app.listen(5000,()=>{
   console.log("server is listeing at port 5000");
 })
--- Api_intro/middleware2_authorize.js
+++ Api_intro/middleware2_authorize.js
@@ -1,5 +1,16 @@
 const authorize=((req,res,next)=>{
     console.log("Authorize");
+    const {user}=req.query;
+    if (user=="nilesh"){ // here we are creating an user this user can accessed by any of 
+        // of the route if auth is successful
+        console.log("entered");
+        req.user={name:"nilesh",id:1};
         next();
+    }
+    else{
+        console.log("denied");
+        res.status(400).send("Unauthorized");
+    }
+
 });
 module.exports=authorize;
